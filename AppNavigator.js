b0dcfdb22554d38eb15af927de5b20e9a1368d07
jupitergod <jupiter.fierce.31@gmail.com>
2018-12-10 22:38:18 +0700

    refactory

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,8 +1,42 @@
 import { StackNavigator } from 'react-navigation';
-import ScanScreen from './src/screens/ScanScreen';
+import ScanScreen from './src/modules/QRSacnModule/ScanScreen';
+import { LoginForm, FirstScreen, SignupForm } from './src/modules/LoginModule'
+const AppNavigator = StackNavigator(
+  {
+    First: {
+      screen: FirstScreen,
+      navigationOptions: {
+        header: null,
+        gesturesEnabled: false
+      }
+    },
+    Login: {
+      screen: LoginForm,
+      navigationOptions: {
+        header: null,
+        gesturesEnabled: false
+      }
+    },
+    Signup: {
+      screen: SignupForm,
+      navigationOptions: {
+        header: null,
+        gesturesEnabled: false
+      }
+    },
+    Scan: {
+      screen: ScanScreen,
+      navigationOptions: {
+        header: null,
+        gesturesEnabled: false
+      }
+    }
 
-const AppNavigator = StackNavigator({
-  Scan: { screen: ScanScreen }
-});
+  },
+  {
+    headerMode: "screen",
+    initialRouteName: "First"
+  }
+);
 
 export default AppNavigator;
